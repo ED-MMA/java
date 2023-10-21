d648a089560819e1494094ed3506e4a0e04a9eda
Levente Laszlo Papp <leventelaszlo.papp@kuka.com>
2023-10-21 16:53:28 +0200

    Typo in name.

--- src/main/java/com/github/aklakina/edmma/database/orms/SourceFaction.java
+++ src/main/java/com/github/aklakina/edmma/database/orms/SourceFaction.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "SOURCESTATION", schema = "ED")
+@Table(name = "SOURCEFACTION", schema = "ED")
 public class SourceFaction {
     public SourceFaction() {
     }
